17dc7a991ec1f33649f491608ee9a6c51a407a10
Bertók Attila <bertok.attila99@gmail.com>
2021-03-12 15:36:36 +0100

    Worker, Robot, Settler, BillOfMaterials

--- src/Robot.java
+++ src/Robot.java
@@ -1,2 +1,24 @@
-public class Robot {
+public class Robot extends Worker implements ISteppable{
+
+    private final AsteroidField field;
+
+    public Robot(AsteroidField field){
+        this.field = field;
+    }
+
+    @Override
+    public void Die(){
+        System.out.println("Robot.Die()");
+    }
+    @Override
+    public void Explode(){
+        System.out.println("Robot.Explode()");
+    }
+
+    //@Override
+    public void Step(){
+        System.out.println("Robot.Explode()");
+    }
+
+
 }
--- src/Settler.java
+++ src/Settler.java
@@ -1,2 +1,51 @@
-public class Settler {
+import java.util.ArrayList;
+
+public class Settler extends Worker implements ISteppable{
+
+    private final AsteroidField field;
+
+    private boolean canStep;
+
+    private ArrayList<Material> backpack;
+
+    private ArrayList<TeleportGate> gateInventory;
+
+    public Settler(AsteroidField field){
+        this.field = field;
+        canStep = false;
+        backpack = new ArrayList<>();
+        gateInventory = new ArrayList<>();
+    }
+
+    public void  PlaceMaterial(Material m){
+        System.out.println("Settler.PlaceMaterial()");
+    }
+
+    public void  PlaceGate(TeleportGate t){
+        System.out.println("Settler.PlaceGate()");
+    }
+
+    public void  CraftGate(){
+        System.out.println("Settler.CraftGate()");
+    }
+
+    public void  BuildRobot(){
+        System.out.println("Settler.BuildRobot()");
+    }
+
+    @Override
+    public void Explode(){
+        System.out.println("Settler.Explode()");
+    }
+
+    public void SkipAction(){
+        System.out.println("Settler.SkipAction()");
+    }
+
+    @Override
+    public void Die(){
+        System.out.println("Settler.Die()");
+    }
+
+
 }

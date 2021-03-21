18d14b714f4a576b30d726fd471a6ca331ebab0b
domonkos2000 <domonkos2000@gmail.com>
2021-03-21 21:12:32 +0100

    kommentek

--- src/me/piedpiper/businesslogic/Iron.java
+++ src/me/piedpiper/businesslogic/Iron.java
@@ -1,10 +1,14 @@
 package me.piedpiper.businesslogic;
 
 public class Iron extends Material {
+    //Az osztaly konstruktora
     public Iron(){
         Logger.logMessage("Iron#" + Integer.toHexString(this.hashCode()) + ".Ctor()");
         Logger.tabcount--;
     }
+
+    // Polimorf viselkedes megvalositasa
+    // megmondja, hogy a kapott anyag is vas-e
     @Override
     public boolean IsCompatibleWith(Material m){
         Logger.logMessage("Iron#" + Integer.toHexString(this.hashCode()) + ".IsCompatibleWith()");
--- src/me/piedpiper/businesslogic/Point2D.java
+++ src/me/piedpiper/businesslogic/Point2D.java
@@ -4,6 +4,7 @@ public class Point2D {
     private double x;
     private double y;
 
+    //Konstruktor
     public Point2D(double x, double y){
         Logger.logMessage("Point2D#" + Integer.toHexString(this.hashCode()) + ".Ctor()");
         
@@ -13,18 +14,21 @@ public class Point2D {
         Logger.tabcount--;
     }
 
+    //A fuggveny visszaadja az x attributumot
     public double GetX(){ 
         Logger.logMessage("Point2D#" + Integer.toHexString(this.hashCode()) + ".GetX()");
         Logger.tabcount--;
         return x;
     }
 
+    //A fuggveny visszaadja az y attributumot
     public double GetY(){ 
         Logger.logMessage("Point2D#" + Integer.toHexString(this.hashCode()) + ".GetY()");
         Logger.tabcount--;
         return y;
     }
 
+    //A fuggveny beallitja az x attributumot
     public void SetX(int x){ 
         Logger.logMessage("Point2D#" + Integer.toHexString(this.hashCode()) + ".SetX()");
         
@@ -33,6 +37,7 @@ public class Point2D {
         Logger.tabcount--;
     }
 
+    //A fuggveny beallitja az y attributumot
     public void SetY(int y){ 
         Logger.logMessage("Point2D#" + Integer.toHexString(this.hashCode()) + ".Ctor()");
         
@@ -41,6 +46,7 @@ public class Point2D {
         Logger.tabcount--;
     }
 
+    //A fuggveny megadja milyen tavol van a parameterkent kapott pont
     public double DistanceFrom(Point2D p) {
         Logger.logMessage("Point2D#" + Integer.toHexString(this.hashCode()) + ".DistanceFrom()");
         Logger.tabcount--;

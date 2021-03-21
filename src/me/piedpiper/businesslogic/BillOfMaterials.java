97577141ec1372d86a2e22bdc5a8f9520b4b2fd6
Mihály Tass <tassmihaly@edu.bme.hu>
2021-03-21 21:00:05 +0100

    Game, Asteroid, BillOfMaterial comments

--- src/me/piedpiper/businesslogic/BillOfMaterials.java
+++ src/me/piedpiper/businesslogic/BillOfMaterials.java
@@ -2,19 +2,24 @@ package me.piedpiper.businesslogic;
 
 import java.util.ArrayList;
 
+//Egy nyeranyagokból álló receptet lehet létrehozni vele,
+// segítségével ellenőrízhető, hogy egy entitás rendelkezik-e
+//bizonyos nyersanyagokkal
 public class BillOfMaterials {
+    //A recept nyersanyag listája
     private  ArrayList<Material> bill;
-    private  ArrayList<Material> cache;
 
+    //Konstruktor
     public BillOfMaterials(ArrayList<Material> bill){
         Logger.logMessage("BillOfMaterials#" + Integer.toHexString(this.hashCode()) + ".Ctor()");
 
         this.bill = bill;
-        this.cache = (ArrayList<Material>)bill.clone();
 
         Logger.tabcount--;
     }
 
+    //Ellenőrzi, hogy az adott nyersanyag benne van-e a listában,
+    //ha igen, akkor kiveszi a listából és igazzal tér vissza
     public boolean IsNeeded(Material m){
         Logger.logMessage("BillOfMaterials#" + Integer.toHexString(this.hashCode()) + ".IsNeeded()");
         
@@ -31,14 +36,11 @@ public class BillOfMaterials {
         return false;
     }
 
+    //recept lista getter
     public ArrayList<Material> GetBill() {
         Logger.logMessage("BillOfMaterials#" + Integer.toHexString(this.hashCode()) + ".GetBill()");
         Logger.tabcount--;
         return bill;
-    }
-
-    public void Init(){
-        bill = (ArrayList<Material>)cache.clone();
     }
 
 }

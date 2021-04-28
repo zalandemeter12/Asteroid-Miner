962529452aa6538b927526ed1405ba2c1413e018
Máté Meglécz <meglecz.mate@gmail.com>
2021-04-28 20:45:36 +0200

    AsteroidPanel, BaseAsteroidPanel, EllipsePanel kész?

--- src/me/piedpiper/gui/AsteroidPanel.java
+++ src/me/piedpiper/gui/AsteroidPanel.java
@@ -4,6 +4,7 @@ import me.piedpiper.businesslogic.Asteroid;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 
 public class AsteroidPanel extends JPanel {
     private Asteroid asteroid;
@@ -11,5 +12,10 @@ public class AsteroidPanel extends JPanel {
     @Override
     public void paintComponents(Graphics g) {
         super.paintComponents(g);
+
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setPaint(Color.gray);
+        g2d.fillOval((int)asteroid.GetPosition().GetX(), (int)asteroid.GetPosition().GetY(), 10, 10);
+
     }
 }
